2479d6f12151f25c64d233c913adaeb0b0837459
Marlin Watts-Woods <Hotspotmar@gmail.com>
2023-05-16 19:25:07 -0700

    Added Project Euler 11 to main menu and made sure it actually worked.

--- src/example/marley/MainMenu.java
+++ src/example/marley/MainMenu.java
@@ -1,8 +1,11 @@
 package example.marley;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import example.marley.ProjectEuler_1;
 import example.marley.ProjectEuler_2;
 //import example.marley.ProjectEuler_4;
+import example.marley.ProjectEuler_11;
 
 public class MainMenu {
 	public static void main(String[] args)
@@ -61,13 +64,65 @@ public class MainMenu {
 				System.out.println("The result is: " + operation.findPalindrome());
 				
 			}
+			else if(input.equals("Euler_11"))
+			{
+				ArrayList<Integer> row1 = new ArrayList<>(Arrays.asList(8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8));
+				ArrayList<Integer> row2 = new ArrayList<>(Arrays.asList(49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0));
+				ArrayList<Integer> row3 = new ArrayList<>(Arrays.asList(81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65));
+				ArrayList<Integer> row4 = new ArrayList<>(Arrays.asList(52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91));
+				ArrayList<Integer> row5 = new ArrayList<>(Arrays.asList(22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80));
+				
+				ArrayList<Integer> row6 = new ArrayList<>(Arrays.asList(24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50));
+				ArrayList<Integer> row7 = new ArrayList<>(Arrays.asList(32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70));
+				ArrayList<Integer> row8 = new ArrayList<>(Arrays.asList(67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21));
+				ArrayList<Integer> row9 = new ArrayList<>(Arrays.asList(24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72));
+				ArrayList<Integer> row10 = new ArrayList<>(Arrays.asList(21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95));
+				
+				ArrayList<Integer> row11 = new ArrayList<>(Arrays.asList(78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92));
+				ArrayList<Integer> row12 = new ArrayList<>(Arrays.asList(16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57));
+				ArrayList<Integer> row13 = new ArrayList<>(Arrays.asList(86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58));
+				ArrayList<Integer> row14 = new ArrayList<>(Arrays.asList(19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40));
+				ArrayList<Integer> row15 = new ArrayList<>(Arrays.asList(4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66));
+				
+				ArrayList<Integer> row16 = new ArrayList<>(Arrays.asList(88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69));
+				ArrayList<Integer> row17 = new ArrayList<>(Arrays.asList(4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36));
+				ArrayList<Integer> row18 = new ArrayList<>(Arrays.asList(20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16));
+				ArrayList<Integer> row19 = new ArrayList<>(Arrays.asList(20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54));
+				ArrayList<Integer> row20 = new ArrayList<>(Arrays.asList(1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48));
+				
+				ArrayList<ArrayList<Integer>> Grid = new ArrayList<ArrayList<Integer>>();
+
+				Grid.add(row1);
+				Grid.add(row2);
+				Grid.add(row3);
+				Grid.add(row4);
+				Grid.add(row5);
+				Grid.add(row6);
+				Grid.add(row7);
+				Grid.add(row8);
+				Grid.add(row9);
+				Grid.add(row10);
+				Grid.add(row11);
+				Grid.add(row12);
+				Grid.add(row13);
+				Grid.add(row14);
+				Grid.add(row15);
+				Grid.add(row16);
+				Grid.add(row17);
+				Grid.add(row18);
+				Grid.add(row19);
+				Grid.add(row20);
+				
+				ProjectEuler_11 operation = new ProjectEuler_11();
+				System.out.println("The answer is: " + operation.solveGrid(Grid, 4));
+			}
 			else if(input.equalsIgnoreCase("quit"))
 			{
 				break;
 			}
 		}
 		
-		System.out.println("Exiting progrram.");
+		System.out.println("Exiting program.");
 		scanner.close();
 	}
 }
--- src/example/marley/ProjectEuler_11.java
+++ src/example/marley/ProjectEuler_11.java
@@ -8,32 +8,41 @@ public class ProjectEuler_11 {
 		if(Grid.size() < lineLength || Grid.get(0).size() < lineLength)
 		{
 			System.out.println("The grid must be at least " + lineLength + "x" + lineLength);
+			return 0;
 		}
 		
 		int biggestNum = 0;
+		ArrayList<Integer> biggestNums = new ArrayList<Integer>();
+		String direction = "none";
 
 		for(int i = 0; i < Grid.size() - lineLength; ++i)
 		{
 			for(int j = 0; j < Grid.get(0).size() - lineLength; ++j)
 			{
 				int iProduct = 0;
+				ArrayList<Integer> iNumArray = new ArrayList<Integer>();
 				int jProduct = 0;
+				ArrayList<Integer> jNumArray = new ArrayList<Integer>();
 				int kProduct = 0;
+				ArrayList<Integer> kNumArray = new ArrayList<Integer>();
 				int negkProduct = 0;
+				ArrayList<Integer> negkNumArray = new ArrayList<Integer>();
 				if(Grid.size() > i + lineLength)
 				{
 					++iProduct;
 					for(int k = 0; k < lineLength; ++k)
 					{
 						iProduct = iProduct * Grid.get(i + k).get(j);
+						iNumArray.add(Grid.get(i + k).get(j));
 					}
 				}
-				if(Grid.get(i).size() > i + lineLength)
+				if(Grid.get(i).size() > j + lineLength)
 				{
 					++jProduct;
 					for(int k = 0; k < lineLength; ++k)
 					{
 						jProduct = jProduct * Grid.get(i).get(j + k);
+						jNumArray.add(Grid.get(i).get(j + k));
 					}
 				}
 				if(kProduct != 0 && kProduct != 0)
@@ -42,35 +51,57 @@ public class ProjectEuler_11 {
 					for(int k = 0; k < lineLength; ++k)
 					{
 						kProduct = kProduct * Grid.get(i + k).get(j + k);
+						kNumArray.add(Grid.get(i + k).get(j + k));
 					}
 				}
-				if(i - lineLength >= 0 && j - lineLength >= 0)
+				if(i - lineLength - 1  >= 0 && j - lineLength - 1 >= 0)
 				{
 					++negkProduct;
 					for(int k = 0; k < lineLength; ++k)
 					{
-						negkProduct = negkProduct * Grid.get(i + k).get(j + k);
+						negkProduct = negkProduct * Grid.get(i + k).get(j - k);
+						negkNumArray.add(Grid.get(i + k).get(j - k));
 					}
 				}
 				if(iProduct > biggestNum)
 				{
 					biggestNum = iProduct;
+					biggestNums = iNumArray;
+					direction = "vertical";
+					//Vertical
 				}
 				if(jProduct > biggestNum)
 				{
 					biggestNum = jProduct;
+					biggestNums = jNumArray;
+					direction = "horizonatal";
+					//Horizontal
 				}
 				if(kProduct > biggestNum)
 				{
 					biggestNum = kProduct;
+					biggestNums = kNumArray;
+					direction = "Ascending Diagonal";
+					//Forward Diagonal
 				}
 				if(negkProduct > biggestNum)
 				{
 					biggestNum = negkProduct;
+					biggestNums = negkNumArray;
+					direction = "Decending Diagonal";
+					//Backwards Diagonal
 				}
-
 			}
 		}
-		return 0;
+		System.out.println("Biggest Nums: " 
+		+ Integer.toString(biggestNums.get(0)) + ", "
+		+ Integer.toString(biggestNums.get(1)) + ", "
+		+ Integer.toString(biggestNums.get(2)) + ", "
+		+ Integer.toString(biggestNums.get(3))
+		);
+
+		System.out.println("the direction is: " + direction);
+		
+		return biggestNum;
 	}
 }
